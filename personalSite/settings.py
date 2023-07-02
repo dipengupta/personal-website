faf09d92795d312af5d1b4ab761590efcca8f454
dipengupta <dipenrgupta@gmail.com>
2023-07-02 11:57:46 -0400

    vercel pt9

--- personalSite/settings.py
+++ personalSite/settings.py
@@ -79,17 +79,9 @@ WSGI_APPLICATION = 'personalSite.wsgi.app'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+
 }
 
-
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-DATABASES['default']['OPTIONS']['charset'] = 'utf8mb4'
-del DATABASES['default']['OPTIONS']['sslmode'] 
-DATABASES['default']['OPTIONS']['ssl'] =  {'ca': os.environ.get('MYSQL_ATTR_SSL_CA')}
 
 
 
