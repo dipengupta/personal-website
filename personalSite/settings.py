ff335aa0f4ac42a020601a0adc6554f60647583f
dipeng <dipeng@urlinsgroup.com>
2024-03-24 17:08:34 -0400

    added logos for medium and threads

--- personalSite/settings.py
+++ personalSite/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z^153y2g@x2_y!_!+a$2^8nt%t=ry$wtw421z5n$(a&+@$ivjn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+#IMP: if this is false, images don't show up in local!!
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','dipengupta5517.pythonanywhere.com']
 
